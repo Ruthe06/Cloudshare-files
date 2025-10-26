ecdf379db58607f6dd2e4415ee71caa09e056344
v23982445-droid <v23982445@gmail.com>
2025-10-26 12:27:42 +0530

    fix: increase MAX_BUFFER_SIZE for big files

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   TEMP_DIR: './temp',
   UPLOAD_DIR: './uploads',
   LOG_DIR: './logs',
-  MAX_BUFFER_SIZE: 2 * 1024 * 1024,
-  MAX_FILE_SIZE: 2 * 1024 * 1024 * 1024,
-  CHUNK_SIZE: 10 * 1024 * 1024,
+  MAX_BUFFER_SIZE: 10 * 1024 * 1024,
+  MAX_FILE_SIZE: 10 * 1024 * 1024 * 1024,
+  CHUNK_SIZE: 5 * 1024 * 1024,
   CLEANUP_INTERVAL: 60 * 60 * 1000
 };
